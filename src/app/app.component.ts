b1e08483063566efb0cf51fc7366c7ff0fc6dac4
amaurylekens <amaury.lekens@gmail.com>
2020-02-22 12:14:18 +0100

    affich

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
   }
 
   getData(){
-    this.apiAccess.getMatch('1ebd0cee-54c0-11ea-9411-2cf0ee2d2352').subscribe((match) => {
+    this.apiAccess.getMatch('96f9eb34-5563-11ea-b53f-2cf0ee2d2352').subscribe((match) => {
       this.match = match;
       console.log(match)
     });
